6f30e9996f0a47b28387b7dab52d9b5dbd18e13c
Warren Pan <wpan.nysh@gmail.com>
2025-06-11 00:31:28 -0700

    turbo

---
+++ apps/web/.next/static/chunks/pages/_app.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/[root-of-the-server]__e2c08166._.js",
+  "static/chunks/node_modules_react-dom_82bb97c6._.js",
+  "static/chunks/node_modules_a51498a5._.js",
+  "static/chunks/[root-of-the-server]__20ba2906._.js",
+  "static/chunks/apps_web_pages__app_5771e187._.js",
+  "static/chunks/apps_web_pages__app_84ad9cf5._.js"
+])
--- packages/ui/dist/button.js
+++ packages/ui/dist/button.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Button = void 0;
+const jsx_runtime_1 = require("react/jsx-runtime");
 const Button = ({ children, onClick, variant = 'primary', disabled = false, className = '' }) => {
     const baseClasses = 'px-4 py-2 rounded font-medium transition-colors';
     const variantClasses = {
@@ -9,13 +10,6 @@ const Button = ({ children, onClick, variant = 'primary', disabled = false, clas
     };
     const disabledClasses = disabled ? 'opacity-50 cursor-not-allowed' : 'cursor-pointer';
     const buttonClasses = `${baseClasses} ${variantClasses[variant]} ${disabledClasses} ${className}`;
-    return className = { buttonClasses };
-    onClick = { onClick };
-    disabled = { disabled }
-        >
-            { children }
-        < /button>;
+    return ((0, jsx_runtime_1.jsx)("button", { className: buttonClasses, onClick: onClick, disabled: disabled, children: children }));
 };
 exports.Button = Button;
-;
-;
